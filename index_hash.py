9fcc0276879c1533a7c03722d972abda29e29ba5
Elena Vuceljic <>
2019-04-09 21:38:08 +0200

    Index hash optimisation

--- index_hash.py
+++ index_hash.py
@@ -15,7 +15,7 @@ class IndexHash(AlgorithmWithIndexStructure):
 
         init_progress = ProgressBar(len(self.__text), "Adding indexes to hash table...")
 
-        for i in range(len(self.__text) - IndexHash.__pattern_len + 1):
+        for i in range(len(self.__text)):
             substring = self.__text[i:i + IndexHash.__pattern_len]
             if substring in self.__index:
                 self.__index[substring].append(i)  # substring already in dictionary
@@ -30,24 +30,28 @@ class IndexHash(AlgorithmWithIndexStructure):
         self.__index = {}
 
     def query(self, pattern):
-
         if len(pattern) == 0:
             return []
         else:
             query_progress = ProgressBar(len(pattern), "Running query...")
 
-            # include all offsets initially
-            results = [x for x in range(0, len(self.__text))]
+            results = []
 
             # find offsets of each substring in pattern less or equal to default pattern length
             for i in range(0, len(pattern), IndexHash.__pattern_len):
                 query_progress.update_progress(i)
                 substring = pattern[i:i + IndexHash.__pattern_len]
                 if len(substring) == IndexHash.__pattern_len and substring in self.__index:
-                    # check if current substring offset is immediately after the previous substring offset
-                    results = list(filter(lambda x: (x + i) in self.__index[substring], results))
+                    if i == 0:
+                        results = list(self.__index[substring])
+                    else:
+                        # check if current substring offset is immediately after the previous substring offset
+                        results = list(filter(lambda x: (x + i) in self.__index[substring], results))
                 else:
-                    results = list(filter(lambda x: self.__text[(x + i):(x + i + len(substring))] == substring, results))
+                    if i == 0:
+                        results = [value for key in self.__index.keys() if key.startswith(substring) for value in self.__index[key]]
+                    else:
+                        results = list(filter(lambda x: self.__text[(x + i):(x + i + len(substring))] == substring, results))
 
                 # return if there are no results
                 if len(results) == 0:
